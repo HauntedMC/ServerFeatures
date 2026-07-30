85ca1c15e6353aae781282e190987f73f07a6494
Remy Duijsens <remyduijsens@gmail.com>
2026-07-30 08:48:20 +0200

    style(invtools): remove unused Mockito import

--- serverfeatures-platform-paper/src/test/java/nl/hauntedmc/serverfeatures/features/invtools/gui/InvToolsViewOfflineCursorTest.java
+++ serverfeatures-platform-paper/src/test/java/nl/hauntedmc/serverfeatures/features/invtools/gui/InvToolsViewOfflineCursorTest.java
@@ -34,7 +34,6 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.mockConstruction;
