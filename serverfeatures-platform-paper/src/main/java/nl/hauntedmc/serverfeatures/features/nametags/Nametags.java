05e3d7f7867464f827d8a0b0763dd3bff6aa8ddb
Remy Duijsens <remyduijsens@gmail.com>
2026-07-31 00:29:02 +0200

    Wire reliable nametag lifecycle and recovery settings

--- serverfeatures-platform-paper/src/main/java/nl/hauntedmc/serverfeatures/features/nametags/Nametags.java
+++ serverfeatures-platform-paper/src/main/java/nl/hauntedmc/serverfeatures/features/nametags/Nametags.java
@@ -1,17 +1,19 @@
 package nl.hauntedmc.serverfeatures.features.nametags;
 
+import com.github.retrooper.packetevents.PacketEvents;
 import nl.hauntedmc.dataprovider.api.orm.ORMContext;
 import nl.hauntedmc.dataprovider.database.DatabaseType;
-import nl.hauntedmc.serverfeatures.features.FeatureContext;
 import nl.hauntedmc.serverfeatures.api.io.config.ConfigMap;
 import nl.hauntedmc.serverfeatures.api.io.localization.MessageMap;
 import nl.hauntedmc.serverfeatures.features.BukkitBaseFeature;
+import nl.hauntedmc.serverfeatures.features.FeatureContext;
 import nl.hauntedmc.serverfeatures.features.nametags.command.NametagCommand;
 import nl.hauntedmc.serverfeatures.features.nametags.entities.PlayerNametagEntity;
-import nl.hauntedmc.serverfeatures.features.nametags.internal.NametagManager;
 import nl.hauntedmc.serverfeatures.features.nametags.internal.NametagDBService;
+import nl.hauntedmc.serverfeatures.features.nametags.internal.NametagManager;
 import nl.hauntedmc.serverfeatures.features.nametags.internal.hook.LuckPermsHook;
 import nl.hauntedmc.serverfeatures.features.nametags.internal.hook.PlaceholderHook;
+import nl.hauntedmc.serverfeatures.features.nametags.internal.packet.NametagPassengerPacketListener;
 import nl.hauntedmc.serverfeatures.features.nametags.listener.NametagListener;
 import nl.hauntedmc.serverfeatures.features.nametags.meta.Meta;
 import org.bukkit.Bukkit;
@@ -20,6 +22,8 @@ public class Nametags extends BukkitBaseFeature<Meta> {
     private NametagManager nametagManager;
     private NametagDBService repository;
     private ORMContext ormContext;
+    private PlaceholderHook placeholderHook;
+    private NametagPassengerPacketListener passengerPacketListener;
 
     public Nametags(FeatureContext<Meta> context) {
         super(context);
@@ -29,10 +33,20 @@ public class Nametags extends BukkitBaseFeature<Meta> {
     public ConfigMap getDefaultConfig() {
         ConfigMap defaults = new ConfigMap();
         defaults.put("enabled", false);
-        defaults.put("update_interval_ticks", 2);
-        defaults.put("viewer_update_delay_ticks", 10);
         defaults.put("max_distance", 45);
 
+        defaults.put("lifecycle.join_settle_delay_ticks", 10);
+        defaults.put("lifecycle.tracking_settle_delay_ticks", 2);
+        defaults.put("lifecycle.transition_settle_delay_ticks", 10);
+        defaults.put("lifecycle.teleport_rebuild_distance", 64);
+
+        defaults.put("reconciliation.interval_ticks", 10);
+        defaults.put("repair.remount_enabled", true);
+        defaults.put("repair.remount_interval_ticks", 100);
+
+        // Retained so existing installations do not lose documented settings during migration.
+        defaults.put("update_interval_ticks", 2);
+        defaults.put("viewer_update_delay_ticks", 10);
         defaults.put("remount_fix.enabled", true);
         defaults.put("remount_fix.interval_ticks", 10);
         defaults.put("debounce_update_ticks", 5);
@@ -52,18 +66,21 @@ public class Nametags extends BukkitBaseFeature<Meta> {
         messages.add("nametags.selfview.status_on", "&7Eigen nametag weergave is &aingeschakeld&7.");
         messages.add("nametags.selfview.status_off", "&7Eigen nametag weergave is &cuitgeschakeld&7.");
         messages.add("nametags.selfview.usage", "&7Gebruik: /nametag selfview on|off|toggle|status");
-        messages.add("nametags.selfview.already_enabled", "&7Eigen nametag weergave is al &aingeschakeld7.");
+        messages.add("nametags.selfview.already_enabled", "&7Eigen nametag weergave is al &aingeschakeld&7.");
         messages.add("nametags.selfview.already_disabled", "&7Eigen nametag weergave is al &cuitgeschakeld&7.");
         return messages;
     }
 
     @Override
     public void initialize() {
-        new PlaceholderHook(this);
+        this.placeholderHook = new PlaceholderHook(this);
 
-        // Data layer (ORM)
         getLifecycleManager().getDataManager().initDataProvider(getFeatureName());
-        getLifecycleManager().getDataManager().registerConnection("ormConnection", DatabaseType.MYSQL, "player_data_rw");
+        getLifecycleManager().getDataManager().registerConnection(
+                "ormConnection",
+                DatabaseType.MYSQL,
+                "player_data_rw"
+        );
         this.ormContext = getLifecycleManager().getDataManager()
                 .createORMContext("ormConnection", PlayerNametagEntity.class)
                 .orElseThrow(() -> new IllegalStateException(
@@ -71,23 +88,34 @@ public class Nametags extends BukkitBaseFeature<Meta> {
                 ));
         this.repository = new NametagDBService(this);
 
-        // Runtime manager
         this.nametagManager = NametagManager.create(this);
-        this.nametagManager.initializeOnlinePlayers();
+        this.passengerPacketListener = new NametagPassengerPacketListener(
+                nametagManager.getAttachmentIndex()
+        );
+        PacketEvents.getAPI().getEventManager().registerListener(passengerPacketListener);
 
-        // Listeners & commands
         getLifecycleManager().getListenerManager().registerListener(new NametagListener(this));
         getLifecycleManager().getCommandManager().registerBrigadierCommand(new NametagCommand(this));
 
         if (Bukkit.getPluginManager().isPluginEnabled("LuckPerms")) {
             LuckPermsHook.subscribeLuckPermsHook(this);
         }
+
+        this.nametagManager.initializeOnlinePlayers();
     }
 
     @Override
     public void disable() {
-        if (this.nametagManager != null) {
-            this.nametagManager.removeAllNametags();
+        if (passengerPacketListener != null) {
+            PacketEvents.getAPI().getEventManager().unregisterListener(passengerPacketListener);
+            passengerPacketListener = null;
+        }
+        if (nametagManager != null) {
+            nametagManager.removeAllNametags();
+        }
+        if (placeholderHook != null) {
+            placeholderHook.close();
+            placeholderHook = null;
         }
     }
 
