deb714faf5c01087e6b064b59d1376380b06fdfd
Remy Duijsens <remyduijsens@gmail.com>
2026-08-02 14:25:13 +0200

    Clean AutoPickup committer imports

--- serverfeatures-platform-paper/src/main/java/nl/hauntedmc/serverfeatures/features/autopickup/transfer/AutoPickupTransferCommitter.java
+++ serverfeatures-platform-paper/src/main/java/nl/hauntedmc/serverfeatures/features/autopickup/transfer/AutoPickupTransferCommitter.java
@@ -6,7 +6,6 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.IdentityHashMap;
 import java.util.List;
 import java.util.Map;
