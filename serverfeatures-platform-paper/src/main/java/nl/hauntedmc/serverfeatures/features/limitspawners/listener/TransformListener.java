5a3ff1219445720ab8c1b080b6a6a48700a13ba9
Remy Duijsens <remyduijsens@gmail.com>
2026-08-03 22:35:52 +0200

    Enforce transformed and multiplying descendants

--- serverfeatures-platform-paper/src/main/java/nl/hauntedmc/serverfeatures/features/limitspawners/listener/TransformListener.java
+++ serverfeatures-platform-paper/src/main/java/nl/hauntedmc/serverfeatures/features/limitspawners/listener/TransformListener.java
@@ -1,10 +1,12 @@
 package nl.hauntedmc.serverfeatures.features.limitspawners.listener;
 
+import io.papermc.paper.event.entity.ShulkerDuplicateEvent;
 import nl.hauntedmc.serverfeatures.features.limitspawners.internal.LimitSpawnersHandler;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityTransformEvent;
+import org.bukkit.event.entity.SlimeSplitEvent;
 
 public final class TransformListener implements Listener {
 
@@ -14,8 +16,37 @@ public final class TransformListener implements Listener {
         this.handler = handler;
     }
 
-    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onTransform(EntityTransformEvent event) {
-        handler.transferTracking(event.getEntity(), event.getTransformedEntities());
+        if (!handler.reserveTransform(event.getEntity(), event.getTransformedEntities())) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onTransformFinalState(EntityTransformEvent event) {
+        handler.scheduleTransformFinalization(event.getEntity(), event::isCancelled);
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onSlimeSplit(SlimeSplitEvent event) {
+        int accepted = handler.prepareSlimeSplit(event.getEntity(), event.getCount());
+        if (accepted <= 0) {
+            event.setCancelled(true);
+        } else if (accepted != event.getCount()) {
+            event.setCount(accepted);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onShulkerDuplicate(ShulkerDuplicateEvent event) {
+        if (!handler.tryReserveShulkerChild(event.getParent(), event.getEntity())) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onShulkerDuplicateFinalState(ShulkerDuplicateEvent event) {
+        handler.scheduleSpawnFinalization(event.getEntity(), event::isCancelled);
     }
 }
