5d4948f14a2e1232c5b7355db1f5813e4b62ac00
Remy Duijsens <remyduijsens@gmail.com>
2026-08-05 10:38:01 +0200

    Add FairPerks player lifecycle handling

---
+++ serverfeatures-platform-paper/src/main/java/nl/hauntedmc/serverfeatures/features/fairperks/listener/PlayerLifecycleListener.java
@@ -0,0 +1,69 @@
+package nl.hauntedmc.serverfeatures.features.fairperks.listener;
+
+import nl.hauntedmc.serverfeatures.features.fairperks.FairPerks;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.player.PlayerGameModeChangeEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
+
+public final class PlayerLifecycleListener implements Listener {
+
+    private final FairPerks feature;
+
+    public PlayerLifecycleListener(FairPerks feature) {
+        this.feature = feature;
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onJoin(PlayerJoinEvent event) {
+        feature.stateService().initialize(event.getPlayer());
+        feature.getLifecycleManager().getTaskManager().scheduleOneTimeTask(
+                () -> feature.stateService().reconcileEnvironment(event.getPlayer())
+        );
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onQuit(PlayerQuitEvent event) {
+        feature.stateService().remove(event.getPlayer());
+        feature.clearFeedback(event.getPlayer());
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onWorldChange(PlayerChangedWorldEvent event) {
+        feature.stateService().reconcileEnvironment(event.getPlayer());
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onGameModeChange(PlayerGameModeChangeEvent event) {
+        feature.getLifecycleManager().getTaskManager().scheduleOneTimeTask(
+                () -> feature.stateService().reconcileEnvironment(event.getPlayer())
+        );
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onRespawn(PlayerRespawnEvent event) {
+        feature.getLifecycleManager().getTaskManager().scheduleOneTimeTask(
+                () -> feature.stateService().reconcileAfterRespawn(event.getPlayer())
+        );
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onDeath(PlayerDeathEvent event) {
+        feature.stateService().clearFallDamageGrace(event.getEntity());
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onSpawnerMob(CreatureSpawnEvent event) {
+        if (feature.settings().hostiles().markSpawnerMobs()
+                && event.getSpawnReason() == CreatureSpawnEvent.SpawnReason.SPAWNER
+                && feature.hostileClassifier().isHostile(event.getEntity())) {
+            feature.hostileClassifier().markSpawnerMob(event.getEntity());
+        }
+    }
+}
