acd34dd823393411a55c5c908a0946c1208d115d
Remy Duijsens <remyduijsens@gmail.com>
2026-08-05 10:28:33 +0200

    Add FairPerks feature metadata

---
+++ serverfeatures-platform-paper/src/main/java/nl/hauntedmc/serverfeatures/features/fairperks/meta/Meta.java
@@ -0,0 +1,28 @@
+package nl.hauntedmc.serverfeatures.features.fairperks.meta;
+
+import nl.hauntedmc.serverfeatures.api.feature.meta.BaseMeta;
+
+import java.util.List;
+
+public final class Meta implements BaseMeta {
+
+    @Override
+    public String getFeatureName() {
+        return "FairPerks";
+    }
+
+    @Override
+    public String getFeatureVersion() {
+        return "1.0.0";
+    }
+
+    @Override
+    public List<String> getDependencies() {
+        return List.of();
+    }
+
+    @Override
+    public List<String> getPluginDependencies() {
+        return List.of();
+    }
+}
