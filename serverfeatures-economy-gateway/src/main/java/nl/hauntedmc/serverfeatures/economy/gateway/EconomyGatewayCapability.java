e5d62ee854ef4a579e91079d03a27e9bbc3e5e69
Remy Duijsens <remyduijsens@gmail.com>
2026-08-07 14:41:52 +0200

    remove unused modules for eco

--- serverfeatures-economy-gateway/src/main/java/nl/hauntedmc/serverfeatures/economy/gateway/EconomyGatewayCapability.java
+++
@@ -1,7 +0,0 @@
-package nl.hauntedmc.serverfeatures.economy.gateway;
-
-/** Explicit permissions granted to an authenticated external Economy source. */
-public enum EconomyGatewayCapability {
-    CHARGE_AND_DISPATCH,
-    READ_WORKFLOW
-}
