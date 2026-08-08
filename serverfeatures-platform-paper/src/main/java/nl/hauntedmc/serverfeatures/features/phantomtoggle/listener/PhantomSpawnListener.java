94a7e4f42f685a668275eb5f59a551f989e2cb56
Remy Duijsens <remyduijsens@gmail.com>
2026-08-08 10:26:17 +0200

    Block opted-out phantom spawns

---
+++ serverfeatures-platform-paper/src/main/java/nl/hauntedmc/serverfeatures/features/phantomtoggle/listener/PhantomSpawnListener.java
@@ -0,0 +1,30 @@
+package nl.hauntedmc.serverfeatures.features.phantomtoggle.listener;
+
+import com.destroystokyo.paper.event.entity.PhantomPreSpawnEvent;
+import nl.hauntedmc.serverfeatures.features.phantomtoggle.PhantomToggle;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+
+public final class PhantomSpawnListener implements Listener {
+
+    private final PhantomToggle feature;
+
+    public PhantomSpawnListener(PhantomToggle feature) {
+        this.feature = feature;
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
+    public void onPhantomPreSpawn(PhantomPreSpawnEvent event) {
+        if (!(event.getSpawningEntity() instanceof Player player)) {
+            return;
+        }
+        if (!feature.preferences().shouldSuppressSpawn(player)) {
+            return;
+        }
+
+        event.setCancelled(true);
+        event.setShouldAbortSpawn(true);
+    }
+}
