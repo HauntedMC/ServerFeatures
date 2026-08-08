2ef7ffc4948e8257646b872fae23b2ac17b047a1
Remy Duijsens <remyduijsens@gmail.com>
2026-08-08 10:29:47 +0200

    Enforce phantom preference at highest priority

--- serverfeatures-platform-paper/src/main/java/nl/hauntedmc/serverfeatures/features/phantomtoggle/listener/PhantomSpawnListener.java
+++ serverfeatures-platform-paper/src/main/java/nl/hauntedmc/serverfeatures/features/phantomtoggle/listener/PhantomSpawnListener.java
@@ -15,7 +15,7 @@ public final class PhantomSpawnListener implements Listener {
         this.feature = feature;
     }
 
-    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onPhantomPreSpawn(PhantomPreSpawnEvent event) {
         if (!(event.getSpawningEntity() instanceof Player player)) {
             return;
